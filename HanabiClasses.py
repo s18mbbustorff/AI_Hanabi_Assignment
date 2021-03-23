75cc35d0f0780ffb1237946cff6d30ad4cebe10c
s18mbbustorff <78579197+s18mbbustorff@users.noreply.github.com>
2021-03-23 12:21:37 +0100

    HanabiClasses & HanabiMain (previous main and menu) - Improved display + printings

---
+++ HanabiClasses.py
@@ -0,0 +1,520 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  8 15:39:18 2021
+@author: mbust
+"""
+import numpy as np
+from random import *
+import copy
+
+
+class State():
+    def __init__(self, Player1, Player2, deck, playedPile, discardPile, hintTokens, penaltyTokens, turn, parent):
+        self.Player1 = Player1
+        self.Player = self.Player1
+        self.Player2 = Player2
+        self.AI = self.Player2
+        self.deck = deck
+        self.playedPile = playedPile
+        self.discardPile = discardPile
+        self.hintTokens = hintTokens
+        self.penaltyTokens = penaltyTokens
+        self.turn = turn
+        
+        if turn == 1:
+            self.human = True
+        else:
+            self.human = False
+        
+        self.parent = parent
+        self.depth = 0
+        self.value = 0
+        
+    def switchTurn(self):
+        self.turn = self.turn%2 + 1
+        self.human = not self.human
+        
+    def display(self):
+        
+        #show score, tokens and cards left in deck
+        score = 0
+        for s in self.playedPile.piles:
+            score = len(s) + score            
+        print("")
+        print("-----------------------------------------------------------------------------------------")
+        print("")
+        print ("Score : {}. Hint tokens left : {}, Penalty tokens used : {}. Cards left in the deck : {}.\n".format(score, self.hintTokens.numberOfTokens, self.penaltyTokens.numberOfTokens, len(self.deck.cards)))
+
+        red = ';'.join([str(1), str(31), str(28)])
+        blue = ';'.join([str(1), str(34), str(28)])
+        green = ';'.join([str(0), str(30), str(42)])
+        yellow = ';'.join([str(1), str(30), str(43)])
+        white = ';'.join([str(1), str(30), str(47)])
+        colorsList = ["red","blue","green","yellow","white"]
+        colorsVisualList = [red,blue,green,yellow,white]
+        
+        #show cards in the played pile
+        if self.playedPile.numberOfColors > 0:
+            print ("Played Cards : \x1b[%sm %s \x1b[0m" % (red, self.playedPile.convertList(0)), end=" ")
+        if self.playedPile.numberOfColors >= 2:
+            print ('\x1b[%sm %s \x1b[0m' % (blue, self.playedPile.convertList(1)), end=" ")
+        if self.playedPile.numberOfColors >= 3:
+            print ('\x1b[%sm %s \x1b[0m' % (green, self.playedPile.convertList(2)), end=" ")
+        if self.playedPile.numberOfColors >= 4:
+            print ('\x1b[%sm %s \x1b[0m' % (yellow, self.playedPile.convertList(3)), end=" ")
+        if self.playedPile.numberOfColors == 5:
+            print ('\x1b[%sm %s \x1b[0m' % (white, self.playedPile.convertList(4)))
+        print("")
+            
+        #show cards in the discarded pile
+        print ("Discarded Cards : ", end=" ") 
+        discardedPrint = self.discardPile.convertList()
+        for card in discardedPrint:
+            print(card, end="")
+        print("")
+        print("")
+        
+        players = [self.Player1, self.Player2]
+        #show cards in the other player's hand
+        otherPlayer = players[self.turn%2]
+        print("Cards in the other player hand:")
+        #for i in range(len(otherPlayer.cards)):
+        for card in otherPlayer.cards:
+            word = ""
+            indexColor = colorsList.index(card.color)
+            color = colorsVisualList[indexColor]
+            number = str(card.number)
+            if card.colorHinted:
+                number += "*"
+            if card.numberHinted:
+                number += "#"
+            print ('\x1b[%sm %s \x1b[0m' % (color, number), end=" ")
+        print("")
+            
+        
+        #show cards in your own hand
+        activePlayer = players[self.turn-1]
+        print("Cards in your hand:")
+        for card in activePlayer.cards:
+            word = ""
+            indexColor = colorsList.index(card.color)
+            number = "?"
+            color = ';'.join([str(7), str(30), str(47)])
+            if card.numberHinted:
+                number = str(card.number)
+                number += "#"
+            if card.colorHinted:
+                color = colorsVisualList[indexColor]
+                number += "*"
+            
+            print ('\x1b[%sm %s \x1b[0m' % (color, number), end=" ")
+        print("")
+        print(activePlayer.storeInfo())
+        print("")
+        
+    
+                
+class Action():
+    
+    def hint(initialState, hintType, hint):
+        #hintType: String. Can be "number" or "color"
+        #hint: String ("red","green",...) or Int ("1","2","3","4" or "5")
+        
+        #initializing variables (extracted from state)
+        newState = copy.deepcopy(initialState)
+        newState.parent = initialState
+        newState.depth = initialState.depth+1
+        human = newState.human
+        
+        if newState.turn == 1:
+            activePlayer = newState.Player1
+            otherPlayer = newState.Player2
+        elif newState.turn == 2:
+            activePlayer = newState.Player2
+            otherPlayer = newState.Player1
+        hintTokens = newState.hintTokens
+        
+        
+        if hintTokens.numberOfTokens == 0:
+            if human:
+                print ("Error. Number of Hint Tokens is 0. You cannot make a Hint.")
+            return None
+        
+        if otherPlayer.allHintsGiven(hint, hintType):
+            if human:
+                print ("Error. Hint already given or no cards correspond to the hint. You cannot make that hint.")
+            return None
+            
+        for i in range(len(otherPlayer.cards)):
+            if hintType == "color":
+                if otherPlayer.cards[i].color == hint:
+                    otherPlayer.cards[i].colorHinted = True
+            elif hintType == "number":
+                if otherPlayer.cards[i].number == hint:
+                    otherPlayer.cards[i].numberHinted = True
+        
+        hintTokens.removeT(human)
+                    
+        newState.switchTurn()
+        
+        return newState
+        
+    def discard(initialState, cardPosition):
+        #cardPosition: Int. Index of the card you want to discard from your hand.
+        
+        #initializing variables (extracted from state)
+        newState = copy.deepcopy(initialState)
+        newState.parent = initialState
+        newState.depth = initialState.depth+1
+        human = newState.human
+        
+        if newState.turn == 1:
+            activePlayer = newState.Player1
+        elif newState.turn == 2:
+            activePlayer = newState.Player2
+        hintTokens = newState.hintTokens
+        deck = newState.deck
+        discardPile = newState.discardPile
+        
+        if (hintTokens.numberOfTokens == hintTokens.maxTokens):
+            if human:
+                print ("Error. Number of Hint Tokens is maxed. You cannot discard a card.")
+            return None
+        
+        discardedCard = activePlayer.cards[cardPosition]
+        discardPile.addCard(discardedCard)
+        
+        activePlayer.draw(deck, cardPosition)
+        hintTokens.addT(human)
+        
+        newState.switchTurn()
+        
+        return newState
+    
+    
+    
+        
+    def play(initialState, cardPosition):
+        #cardPosition: Int. Index of the card you want to play from your hand.
+        
+        #initializing variables (extracted from state)
+        newState = copy.deepcopy(initialState)
+        newState.parent = initialState
+        newState.depth = initialState.depth+1
+        human = newState.human
+        
+        if newState.turn == 1:
+            activePlayer = newState.Player1
+        elif newState.turn == 2:
+            activePlayer = newState.Player2
+        penaltyTokens = newState.penaltyTokens
+        deck = newState.deck
+        
+        playedCard = activePlayer.cards[cardPosition]
+        
+        #check if the card was correct somehow
+        #use functions from the playPile
+        verif = newState.playedPile.addCard(playedCard, human)
+        
+        if verif == False:
+            newState.penaltyTokens.addT(human)
+            newState.discardPile.addCard(playedCard)
+            if human:
+                print("OOPS. You got a penalty token!")
+            
+        activePlayer.draw(deck, cardPosition)
+        
+        newState.switchTurn()
+            
+        return newState
+
+
+
+class Player():
+    
+    #-----------------------------
+    #-----Initialization functions
+    #-----------------------------
+    
+    def __init__(self,name,order,numberOfCards):
+        self.name = name
+        self.order = order
+        self.cards = [Card(None,0)]*numberOfCards
+        self.numberOfCards = numberOfCards
+        
+    def setCards(self,cards):
+        self.cards(cards)
+        
+    
+    def drawNewHand(self, deck):
+        
+        for i in range(len(self.cards)):
+            self.draw(deck,i)
+        
+    #-----------------------------
+    #-------------Action functions
+    #-----------------------------
+        
+    #-------DISCARD Auxiliary---------------
+    
+    def draw(self,deck, cardPosition):
+        newCard = deck.removeRandom()
+        self.cards[cardPosition] = newCard
+        return newCard
+        
+    #-----------HINT Auxiliary--------------
+    
+    
+    def allHintsGiven(self, hint, hintType):
+        if hint is None:
+            for i in range(len(self.cards)):
+                if (self.cards[i].colorHinted == False) or (self.cards[i].numberHinted == False):
+                    return False
+            return True
+        elif hintType == "color":
+            for i in range(len(self.cards)):
+                if (self.cards[i].color == hint) and (self.cards[i].colorHinted == False):
+                    return False
+            return True
+            
+        elif hintType == "number":
+            for i in range(len(self.cards)):
+                if (self.cards[i].number == hint) and (self.cards[i].numberHinted == False):
+                    return False
+            return True
+        
+    #-----------------------------
+    #------Visualization functions
+    #-----------------------------
+    
+    def storeInfo(self):
+       # Declaring rows 
+       N = len(self.cards)
+       # Declaring columns 
+       M = 2
+       # using list comprehension  
+       # to initializing matrix 
+       cardMatrix = [ [ 0 for i in range(M) ] for j in range(N) ] 
+       
+       for i in range(len(self.cards)):
+           number = str(self.cards[i].number)
+           color = str(self.cards[i].color)
+           if self.cards[i].numberHinted:
+               number = number+"#"
+           if self.cards[i].colorHinted:
+               color = color+"*"
+               
+           cardMatrix[i][0] = number
+           cardMatrix[i][1] = color
+       
+       return cardMatrix
+     
+
+class Card():
+    
+    #-----------------------------
+    #-----Initialization functions
+    #-----------------------------
+    
+    def __init__(self,color,number):
+        self.color = color
+        self.number = number
+        self.colorHinted = False
+        self.numberHinted = False
+    
+    #-----------------------------
+    #------Visualization functions
+    #-----------------------------
+    
+    def sayInfo(self):
+        print ("Color: {}, number: {}.".format(self.color,self.number))
+        
+    def __eq__(self, other):
+       
+        return self.__dict__ == other.__dict__
+
+class Deck():
+    
+    #-----------------------------
+    #-----Initialization functions
+    #-----------------------------
+    
+    def __init__(self,numberOfColors,cardsDistribution):
+        #var cardsDistribution = int list of 1,2,3,4,5s
+        self.numberOfColors = numberOfColors
+        #var numberOfColors = int
+        self.cardsDistribution = cardsDistribution
+        #creating the initial deck of cards with all available cards in it
+        cards=[]
+        cardsPerColor = len(cardsDistribution)
+        colorPossibilities = np.array(["red","blue","green","yellow","white"])[:numberOfColors]
+        cardNumbers = cardsDistribution * numberOfColors
+        for i in range(len(cardNumbers)):
+                color = colorPossibilities[i//cardsPerColor]
+                number = cardNumbers[i]
+                cards.append(Card(color,number))
+            
+        self.cards = cards
+        self.numberOfCards = len(cards)
+        
+    def __intit__(self,cards):
+        self.cards = cards
+        
+    #------------------------------
+    #---Draw/remove cards functions
+    #------------------------------
+    
+    def removeRandom(self):
+        maxIndex =len(self.cards)-1
+        randomIndex =(randrange(maxIndex))
+        randomCard = self.cards[randomIndex]
+        self.cards.pop(randomIndex)
+        self.numberOfCards = len(self.cards)
+        return randomCard
+    
+    def removeCard(self,cardToRemove):
+        for i in range(self.numberOfCards):
+            if cardToRemove.isSameAs(self.cards[i]):
+                del self.cards[i]
+                self.numberOfCards =- 1
+                break
+        return self.numberOfCards
+    
+    #-----------------------------
+    #------Visualization functions
+    #-----------------------------
+    
+    def sayAllCards(self):
+        for i in range(len(self.cards)):
+            self.cards[i].sayInfo()
+            
+    def storeInfo(self):
+        # Declaring rows 
+        N = len(self.cards)
+        # Declaring columns 
+        M = 2
+        # using list comprehension  
+        # to initializing matrix 
+        cardMatrix = [ [ 0 for i in range(M) ] for j in range(N) ] 
+        
+        for i in range(len(self.cards)):
+            cardMatrix[i][0] = self.cards[i].number
+            cardMatrix[i][1] = str(self.cards[i].color)
+        
+        return cardMatrix
+            
+            
+class DiscardPile():
+    
+    def __init__(self, cards):
+        self.cards = cards
+        self.numberOfCards = len(cards)
+        
+    def addCard(self, card):
+        self.cards.append(card)
+        self.numberOfCards += 1
+        return self.numberOfCards
+    
+    def convertList(self):
+        
+        red = ';'.join([str(1), str(31), str(28)])
+        blue = ';'.join([str(1), str(34), str(28)])
+        green = ';'.join([str(0), str(30), str(42)])
+        yellow = ';'.join([str(1), str(30), str(43)])
+        white = ';'.join([str(1), str(30), str(47)])
+        
+        listCards = []
+        for i in range(len(self.cards)):
+            
+            currentCard = self.cards[i]
+            if currentCard.color == "red":
+                coloredCardNumber = '\x1b[%sm %s \x1b[0m' % (red, currentCard.number)
+            elif currentCard.color == "blue":
+                coloredCardNumber = '\x1b[%sm %s \x1b[0m' % (blue, currentCard.number)
+            elif currentCard.color == "green":
+                coloredCardNumber = '\x1b[%sm %s \x1b[0m' % (green, currentCard.number)
+            elif currentCard.color == "yellow":
+                coloredCardNumber = '\x1b[%sm %s \x1b[0m' % (yellow, currentCard.number)
+            elif currentCard.color == "white":
+                coloredCardNumber = '\x1b[%sm %s \x1b[0m' % (white, currentCard.number)
+            listCards += [coloredCardNumber]
+            
+        return listCards
+            
+        
+class PlayedPile():
+    
+    def __init__(self, numberOfColors):
+        self.colors = ["red", "blue", "green", "yellow", "white"][:numberOfColors]
+        self.numberOfColors = numberOfColors
+        self.piles = [[] for i in range(self.numberOfColors)]   ## Lists (representing each pile) within a list 
+        
+    def addCard(self, card, human):
+        if card.color in self.colors:
+            pileIndex = self.colors.index(card.color)
+            
+            if (len(self.piles[pileIndex]) == 0) and (card.number == 1):
+                self.piles[pileIndex].append(card)
+                
+            elif (len(self.piles[pileIndex]) != 0) and (card.number == self.piles[pileIndex][-1].number + 1):
+                self.piles[pileIndex].append(card)
+                
+            else:
+                if human:
+                    print ("Error. Card not matched to any pile. Could not add the card to a pile")
+                return False
+            if human:
+                print("Well done! The card was added to the firework!")
+            return True
+            
+        else:
+            if human:
+                print ("Error. Card color does not exist. Could not add the card to a pile")
+    
+    def convertList(self, pilePosition):
+        listCards = []
+        for i in range(len(self.piles[pilePosition])):
+            listCards += [self.piles[pilePosition][i].number]
+        return listCards
+    
+class HintTokens():
+    
+    def __init__(self, numberOfTokens, maxTokens):
+        self.numberOfTokens = numberOfTokens
+        self.maxTokens = maxTokens
+        
+    def removeT(self, human):
+        if 0 < self.numberOfTokens:
+            self.numberOfTokens -= 1
+        else:
+            if human:
+                print ("Error. Not enough tokens. Could not remove the hint token.")
+       
+    def addT(self, human):
+        if self.numberOfTokens < self.maxTokens:
+            self.numberOfTokens += 1
+        else:
+            if human:
+                print ("Error. Tokens maximum limit reached. Could not add the hint token.")
+        
+        
+class PenaltyTokens():
+    
+    def __init__(self, numberOfTokens, maxTokens):
+        self.numberOfTokens = numberOfTokens
+        self.maxTokens = maxTokens
+        
+    def removeT(self, human):
+        if 0 < self.numberOfTokens:
+            self.numberOfTokens -= 1
+        else:
+            if human:
+                print ("Error. Not enough tokens. Could not remove the penalty token.")
+       
+    def addT(self, human):
+        if self.numberOfTokens < self.maxTokens:
+            self.numberOfTokens += 1
+        else:
+            if human:
+                print ("Error. Tokens maximum limit reached. Could not add the penalty token.")
+    
---
+++ HanabiMain.py
@@ -0,0 +1,257 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 14 16:44:45 2021
+@author: mbust
+"""
+
+from HanabiClasses import *
+from ai_new import Solver
+from inputNumber import inputNumber
+from displayMenu import displayMenu
+from BeliefSpace import BeliefSpace
+
+class CustomError(Exception):
+    pass
+
+def startGame():
+    
+    #create initial state
+    
+    #create Player objects
+    name1 = input("Name of first player: ")
+    """
+    name2 = input("Name of second player: ")
+    """
+    name2 = "AI"
+    order1 = 1
+    order2 = 2
+    numberOfCards = int(input("How many cards per player? "))
+    Player1 = Player(name1,order1,numberOfCards)
+    Player2 = Player(name2,order2,numberOfCards)
+    
+    #create Deck object
+    numberOfColors = int(input("How many colors? "))
+    distributionOptionsList = [[1,2,3,4,5],[1,2,2,3,3,3,4,4,4,4,5,5,5,5,5],[1,1,1,2,2,3,3,4,4,5]]
+    distributionOptionsStr = [str(x) for x in distributionOptionsList]
+    print("Choose card distribution.")
+    choiceDistribution = displayMenu(distributionOptionsStr)
+    cardsDistribution = distributionOptionsList[int(choiceDistribution)-1]
+    deck = Deck(numberOfColors,cardsDistribution)
+            
+    #create Played Pile object
+    playedPile = PlayedPile(numberOfColors)
+    
+    #create Discard Pile object
+    discardPile = DiscardPile([])
+    
+    #create Tokens objects
+    maxTokens = int(input("Choose max number of Hint Tokens: "))
+    hintTokens = HintTokens(maxTokens, maxTokens)
+    
+    maxTokens = int(input("Choose max number of Penalty Tokens: "))
+    penaltyTokens = PenaltyTokens(0, maxTokens)
+    
+    #create final variables for state
+    turn = 1
+    parent = []
+    
+    #create a list of states and store the starting state
+    states = []
+    Player1.drawNewHand(deck)
+    Player2.drawNewHand(deck)
+    initialState = State(Player1, Player2, deck, playedPile, discardPile, hintTokens, penaltyTokens, turn, parent)
+    states.append(initialState)
+    
+    return states
+
+def chooseCardFromHand(activePlayer, otherPlayer, action):
+    
+    if action == "hint":
+        print("{}, what card do you want to {}?".format(activePlayer.name, action))
+        cardOptions = []
+        for i in range(len(otherPlayer.cards)):
+            word2 = str(otherPlayer.cards[i].number)
+            word1 = otherPlayer.cards[i].color
+            
+            if otherPlayer.cards[i].colorHinted:
+                word1 = word1+"*"
+            if otherPlayer.cards[i].numberHinted:
+                word2 = word2+"*"
+                
+                
+            cardInHand = ("Position of the card in hand: {}, color: {}, number: {}".format(i+1, word1, word2))
+            cardOptions.append(cardInHand)
+        cardOptions.append("Go Back")
+        choiceCard = displayMenu(cardOptions)
+        return (choiceCard - 1)
+        
+        
+    else:
+        print("{}, what card do you want to {}?".format(activePlayer.name, action))
+        cardOptions = []
+        for i in range(len(activePlayer.cards)):
+            word1 = "?"
+            word2 = "?"
+            if activePlayer.cards[i].colorHinted:
+                word1 = activePlayer.cards[i].color
+            if activePlayer.cards[i].numberHinted:
+                word2 = str(activePlayer.cards[i].number)
+            cardInHand = ("Position of the card in hand: {}, color: {}, number: {}".format(i+1, word1, word2))
+            cardOptions.append(cardInHand)
+        cardOptions.append("Go Back")
+        choiceCard = displayMenu(cardOptions)
+        return (choiceCard - 1)
+
+def chooseHintType(activePlayer, otherPlayer, cardChoice):
+    
+    cardHinted = otherPlayer.cards[cardChoice]
+    '''
+    print("Here are all cards in that player's hand:")
+    otherPlayer.storeInfo()
+    print("")
+    print("And here is the card you chose:")
+    cardHinted.sayInfo()
+    '''
+    print("")
+    print("{}, what hint would you like to give? (All cards that share that attribute will also be hinted)".format(activePlayer.name))
+    hintOptions = ["color"+" ({})".format(cardHinted.color), "number"+" ({})".format(cardHinted.number), "Go Back"]
+    choiceHint = displayMenu(hintOptions)
+    
+    if choiceHint == 1:
+        hintType = "color"
+        hint = cardHinted.color
+    elif choiceHint == 2:
+        hintType = "number" 
+        hint = cardHinted.number
+    elif choiceHint == 3:
+        hintType = "Go Back"
+        hint = None
+    
+    return hintType, hint
+    
+    
+
+def playRound(states, action, parameter):
+    if isinstance(states,list):
+        initialState = states[-1]
+    else:
+        initialState = states
+    
+    if initialState.turn == 1:
+        activePlayer = initialState.Player1
+        otherPlayer = initialState.Player2
+        human = True
+    elif initialState.turn == 2:
+        activePlayer = initialState.Player2
+        otherPlayer = initialState.Player1
+        human = False
+        
+    
+    actionOptions = ["Play a card", "Give a hint", "Discard a card","Quit"]
+    
+    #ask for user input if human 
+    #get input from computer if AI
+    
+
+
+    while True:
+        
+        #----------------------AI: disable human = true, if you want to test with AI
+        #human = True
+        if human:
+            print("{}'s turn, choose an action: ".format(activePlayer.name))
+            choiceAction = displayMenu(actionOptions)
+        else:
+            solver = Solver(2, 4)
+            space = BeliefSpace(states[-1],len(initialState.AI.cards))
+            choiceAction,parameter = solver.evaluate(space.states, False)
+            print(choiceAction,parameter)
+    
+        #PLAY
+        if choiceAction == 1:
+            if human:
+                cardChoice = int(chooseCardFromHand(activePlayer,None, "play"))
+            else:
+                cardChoice = parameter
+            if cardChoice == 4:
+                newState = None
+            else:
+                newState = Action.play(initialState,cardChoice)
+                break
+        #HINT
+        elif choiceAction == 2:
+            if human:
+                cardChoice = int(chooseCardFromHand(activePlayer, otherPlayer, "hint"))
+                if cardChoice == 4:
+                    newState = None
+                else:
+                    hintType, hint = chooseHintType(activePlayer, otherPlayer, cardChoice)
+                    if hintType == "Go Back":
+                        newState = None
+                    else:
+                        newState = Action.hint(initialState,hintType, hint)
+                        break
+            else:
+                hintType, hint = parameter
+                newState = Action.hint(initialState,hintType, hint)
+                break
+        
+        #DISCARD
+        elif choiceAction == 3:
+            
+            if human:
+                cardChoice = int(chooseCardFromHand(activePlayer, None, "discard"))
+            else:
+                cardChoice = parameter+1
+            
+            if cardChoice == 4:
+                newState = None
+            else:
+                newState = Action.discard(initialState,cardChoice)
+                break
+            
+        #QUIT
+        elif choiceAction == 4:
+            
+            raise CustomError("Program will terminate")
+            break
+            
+    if newState == None:
+        print("Choose another option!")
+        newState = initialState
+
+    if isinstance(states,list):
+        states.append(newState)
+    else:
+        states = newState
+            
+    return states
+        
+    
+def playGame(states):
+    if states == None:
+        states = startGame()
+        
+    
+    while True:
+        if states[-1].human:
+            states[-1].display()
+        else:
+            print("")
+            print("*********************")
+            print("    AI Playing        ")
+            print("*********************")
+        try:
+            #-----------------------------AI: action and parameter must be defined when the AI is playing
+            action = None 
+            parameter = None
+            states = playRound(states, action, parameter)
+            print("Round ended.")
+            
+        except(CustomError):
+            print("Program terminating...")
+            break
+        
+    return states
+if __name__ == "__main__":
+    playGame(None)
